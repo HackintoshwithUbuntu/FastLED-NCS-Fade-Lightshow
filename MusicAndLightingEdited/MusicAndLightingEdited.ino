771639c2edee45effe9c6e0e9589b7cdef56eff0
Syed Ahmad <muzammil.ahmad@gmail.com>
2024-12-21 22:15:39 +1100

    Colours in stage 1 mostly fixed and substage rotation implemented

--- MusicAndLightingEdited/MusicAndLightingEdited.ino
+++ MusicAndLightingEdited/MusicAndLightingEdited.ino
@@ -6,18 +6,17 @@
 
 CRGB leds[NUM_LEDS];
 
-uint8_t currentled = 0;
+uint8_t currentLed = 0;
 uint8_t curStage = 0;
+uint8_t curSubStage = 0;
 
-DEFINE_GRADIENT_PALETTE( stage1_gp ) { 
-  // Stage 1
-  0,    253,  247,  255,    // light purple
-  100,    255,    0,  0,      // red
-  200,   55,  222,   70,      // bright green
-  50,    0,  194,  255       // light blue
-};
-
-CRGBPalette16 stage1Palette(stage1_gp);
+CRGBPalette16 stage1Palette(
+  // Stage 1 Colours
+  0xecbfff,   // Very Light Purple
+  CRGB::Red,
+  CRGB::Green,
+  CRGB(93, 204, 245)    // Light Blue
+);
 
 void setup() {
   FastLED.addLeds<LED_TYPE, LED_PIN, GRB>(leds, NUM_LEDS);
@@ -30,10 +29,17 @@ void loop() {
   //leds[0] = 0xecbfff;
 
   EVERY_N_MILLISECONDS(700) {
-    leds[currentled % NUM_LEDS] = ColorFromPalette(stage1Palette, curStage);
-    leds[0] = 0xecbfff;
-    currentled += 4;
+    leds[currentLed] = ColorFromPalette(stage1Palette, curSubStage * 64); // Settings this multiply to 16 gave some nice gradients
+    currentLed += 4;
+
+    if (currentLed >= NUM_LEDS) {
+      curSubStage++;
+      // Setting the start led to the substage will let us start with an offset
+      currentLed = curSubStage;
+    }
   }
+
+  // Note stage one has 32 beats in total, 33rd is start of next
 
   FastLED.show();
 }
