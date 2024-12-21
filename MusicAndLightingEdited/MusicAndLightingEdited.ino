81d4ef0cec67f8139c66980a9682813d3bd97737
Syed Ahmad <muzammil.ahmad@gmail.com>
2024-12-21 21:53:03 +1100

    Working with inital alignment, some basic colour functionality added

--- MusicAndLightingEdited/MusicAndLightingEdited.ino
+++ MusicAndLightingEdited/MusicAndLightingEdited.ino
@@ -2,21 +2,36 @@
 
 #define NUM_LEDS  18
 #define LED_PIN   2
+#define LED_TYPE  WS2812B
 
 CRGB leds[NUM_LEDS];
 
 uint8_t currentled = 0;
+uint8_t curStage = 0;
+
+DEFINE_GRADIENT_PALETTE( stage1_gp ) { 
+  // Stage 1
+  0,    253,  247,  255,    // light purple
+  100,    255,    0,  0,      // red
+  200,   55,  222,   70,      // bright green
+  50,    0,  194,  255       // light blue
+};
+
+CRGBPalette16 stage1Palette(stage1_gp);
 
 void setup() {
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<LED_TYPE, LED_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(50);
-  Serial.begin(57600);
+  FastLED.setCorrection(TypicalPixelString);
+  Serial.begin(115200);
 }
 
 void loop() {
+  //leds[0] = 0xecbfff;
 
-  EVERY_N_SECONDS(4) {
-    leds[currentled % NUM_LEDS] = CRGB::Blue;
+  EVERY_N_MILLISECONDS(700) {
+    leds[currentled % NUM_LEDS] = ColorFromPalette(stage1Palette, curStage);
+    leds[0] = 0xecbfff;
     currentled += 4;
   }
 
